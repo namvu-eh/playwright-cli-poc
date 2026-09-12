cf2b56d125870d1ff0b83adc4d2f86193467a8fa
Nam Vu <nam.vuthai@employmenthero.com>
2026-09-12 15:59:09 +0700

    test3

---
+++ tests/answer-questions-part3.ts
@@ -0,0 +1,19 @@
+import { runQuiz } from './helpers/quiz-runner';
+
+process.on('uncaughtException', (err) => {
+  console.error('[uncaughtException]', err?.name, err?.message);
+  console.error(err?.stack);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('[unhandledRejection]', reason);
+  process.exit(1);
+});
+
+const userKey = process.argv[2] ?? 'user1';
+runQuiz('Phần 3. Xử lý các tình huống giao thông', 'part3', userKey).catch(err => {
+  console.error('[CRASH]', err?.name, err?.message);
+  console.error(err?.stack);
+  process.exit(1);
+});
